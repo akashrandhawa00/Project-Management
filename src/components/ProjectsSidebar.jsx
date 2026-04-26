8efbc7b8b0c33e8a58c1c9a48c764cbdc1792e60
Akash Randhawa <93700117+akashrandhawa00@users.noreply.github.com>
2026-04-26 19:15:52 -0400

    Initial commit

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,11 @@
+export default function ProjectsSidebar() {
+  return (
+    <aside className="w1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+      <h2 className="mb- 8 font-bold uppercase">Your Projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
