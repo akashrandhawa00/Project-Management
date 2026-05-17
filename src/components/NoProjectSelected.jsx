4fb3700da615d26e0cc2bf1d4821d0d9ca72a377
Akash Randhawa <93700117+akashrandhawa00@users.noreply.github.com>
2026-05-17 18:43:30 -0400

    get user input

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,14 +1,10 @@
 import noProjectImage from "../assets/no-projects.png";
-import Button from "./Button";
+import Button from "./Button.jsx";
 
 export default function NoProjectSelected({ onStartAddProject }) {
   return (
     <div className="mt-24 text-center w-2/3">
-      <img
-        src={noProjectImage}
-        alt="An empty task list"
-        className="w-16 h-16 object-contain mx-auto"
-      />
+      <img src={noProjectImage} className="w-16 h-16 object-contain mx-auto" />
       <h2 className="text-xl font-bold text-stone-500 my-4">
         No Project Selected
       </h2>
